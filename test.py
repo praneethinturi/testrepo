96bd2de4613a8f9f96db168b11619dec49240b34
praneethinturi <71003007+praneethinturi@users.noreply.github.com>
2020-09-09 13:19:57 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+x=5
+y=10
+z=x+y
+if(z==15):
+ print("Correct")
+else:
+ print("Wrong")
+
+if(x==5):
+    print(10*"Correct\n")
+    print("The value of x is 5")
+else:
+    print("The value of x is not 5")
